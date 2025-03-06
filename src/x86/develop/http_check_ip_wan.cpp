1185e4635c861fd4c606fe6ad0da931b5179dfae
dearl <dearl@gmail.com>
2025-03-06 16:22:33 +0000

    update

--- src/x86/develop/http_check_ip_wan.cpp
+++ src/x86/develop/http_check_ip_wan.cpp
@@ -1,30 +1,99 @@
 #include <iostream>
 #include <string.h>
 
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "config.h"
 #include "http_check_ip_wan.h"
 
-
-void check_local_ip()
+typedef struct net_card_s
 {
-    // 检查本地 ip
-    std::cout << "hello c++, long time no see ~ !" << std::endl;
+    int num;
+    char *net_card[10] = {0};
+}net_card_t;
+
+static void ip_local_get();
+
+
+void _IPv6::check_wan_ip()
+{
+    std::cout << "hello c++, check_wan_ip ~ !,love from IPv6" << std::endl;
+}
+
+void Ipaddress::check_local_ip()
+{
+    ip_local_get();
+    std::cout << "hello c++, check_local_ip ~ !,love from IPv6" << std::endl;
+}
+
+void _IPv4::check_wan_ip()
+{
+    std::cout << "hello c++, check_local_ip ~ !,love from IPv6" << std::endl;
+}
+
+void net_hardware_get()
+{
+}
+
+void ip_local_get()
+{
+    char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) == -1)
+    {
+        perror("gethostname");
+        return;
+    }
+
+    struct addrinfo hints{}, *res, *p;
+    hints.ai_family = AF_INET; // 仅获取 IPv4 地址
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(hostname, nullptr, &hints, &res) != 0)
+    {
+        perror("getaddrinfo");
+        return;
+    }
+
+    for (p = res; p != nullptr; p = p->ai_next)
+    {
+        struct sockaddr_in *addr = (struct sockaddr_in *)p->ai_addr;
+        std::cout << "Local IP: " << inet_ntoa(addr->sin_addr) << std::endl;
+    }
+
+    freeaddrinfo(res);
 }
 
 
-void check_wan_ip()
+void ip_wan_http_connect()
 {
-    // 检查是否和 local IPv4 一致
+    // curl 建立连接， 或者依附于全局socket
+
 }
 
 void check_demo_http_check()
 {
 
-
+    std::cout << "===================" << std::endl;
+    ip_local_get();
+    std::cout << "===================" << std::endl;
     // Ipaddress ip(_MACRO_NAS_DOMAIN_URL); // 如果 Ipaddress 父类有虚函数，必须子类实现它之后才能实例化对象
-    _IPv4 * ip_v4 = new _IPv4("1234");
+    _IPv4 *ip_v4 = new _IPv4();
+    Ipaddress *ip_v5 = new _IPv4();
+    Ipaddress *ip_v6 = new _IPv6("1,1,1,1");
     // ip_v4->check_local_ip();
     // ip_v4->check_wan_ip();
+    std::cout << "ip_v4->Ipaddress::domain = " << ip_v4->Ipaddress::domain << std::endl;
+    std::cout << "ip_v4->domain = " << ip_v4->domain << std::endl;
     std::cout << "hello c++, long time no see ~ ip_v4->domain = " << ip_v4->Ipaddress::domain << std::endl;
+    ip_v5->check_wan_ip();
+    ip_v6->check_local_ip();
+    delete ip_v4;
+    delete ip_v5;
+    ip_v4->check_local_ip();
 }
-
--- src/x86/develop/inc/http_check_ip_wan.h
+++ src/x86/develop/inc/http_check_ip_wan.h
@@ -7,19 +7,20 @@ void check_demo_http_check();
 class Ipaddress // 暂不区分 IPv4 和 v6
 {
 public:
-    std::string domain;
+    std::string domain = "123";
     std::string wan_ip;
     bool valid_ip = false;
     // 根据子类的类型来确定用何种ip
-    virtual void check_local_ip() = 0;
+    void check_local_ip();
     virtual void check_wan_ip() = 0;
+
     std::string print_domain() const{ std::cout << domain << std::endl;}
     std::string get_domain() const{ return domain;}
     std::string set_domain(std::string _domain) { this->domain = _domain;}
     // 默认构造函数
     Ipaddress()
     {
-
+        domain = "Ipaddress construction function() ";
     };
     // 有参构造 构造的时候就写入域名
 
@@ -29,33 +30,24 @@ public:
         this->print_domain();
     };
 
-    // 析构, 清理资源 or 释放对象的时候做任何操作。
-    ~Ipaddress()
-    {
-
-    };
-
 };
 
 class _IPv4:public Ipaddress
 {
 public:
+    void check_wan_ip()override;
+    _IPv4(){}
+    ~ _IPv4(){}
+};
 
-    void check_local_ip(){ std::cout << "check_local_ip() func ~ !" << std::endl;};
-    void check_wan_ip(){ std::cout << "check_wan_ip() func ~ !" << std::endl;};
-    _IPv4()
+class _IPv6:public Ipaddress
+{
+public:
+    void check_wan_ip()override;
+    _IPv6(std::string _domain)
     {
-
+        this->domain = _domain;
     }
-
-    _IPv4(std::string _str)
-    {
-        Ipaddress::domain = _str;
-    }
-    ~ _IPv4()
-    {
-    }
-
 };
 
 
